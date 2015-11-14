dbde2cbc5ef33b2119829dd8016a56e83c24954e
Victor Hom <vh62@cornell.edu>
2015-11-14 16:01:28 -0500

    installed bluebird, but wanted to make sure no conflict

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,18 @@ var keys = require("../../keys");
 var https = require('https');
 
 router.get('/', function(req, res){
-	var testResponse = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670,151.1957&radius=500&types=food&name=cruise&key=";
-	console.log(keys.key);
+	var longitude = "-87.622206";
+	var latitude  = "41.888";
+	var address = "151 East Wacker Drive";
+	var city = "Chicago";
+	var radius = "1500";
+	var type = "food";
+	var name = "restaurant";
+	var testResponse = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + latitude + "," + longitude +"&radius=" + radius + "&types=" + type + "&key=";
 	var body = "";
 	https.get(testResponse + keys.key, function(resp){
+		console.log(testResponse + keys.key);
+		// to get next page of results, append to the testResponse + keys.key + &pagetoken=[NEXT PAGE TOKEN GOES HERE]
 		//console.log("in https")
 		//console.log(res.body);
 		//console.log(req.body);
@@ -17,8 +25,9 @@ router.get('/', function(req, res){
   		});
   		resp.on('end', function(){
   			res.header('Content-Type','application/json');
-			res.statusCode = 200;
+				res.statusCode = 200;
   			console.log("on end");
+  			console.log(body);
   			res.send(body);
   			res.end();
   		});
