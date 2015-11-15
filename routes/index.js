c439b7554bbb2d4dc2a43806ffe3485a68dbd8aa
Victor Hom <vh62@cornell.edu>
2015-11-15 03:40:32 -0500

    Fix routes/

--- routes/index.js
+++ routes/index.js
@@ -9,70 +9,68 @@ var request = Promise.promisify(require("request"));
 Promise.promisifyAll(https);
 
 
-function storeSubwaysCloseToHotel(hotel, res, hotels) {
-	var requestUrl = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="
-		+ hotel.latitude + "," + hotel.longitude
-		+"&radius=" + 600
-		+ "&types=" + 'subway_station' +
-		"&key=";
 
 
-	https.get(requestUrl + keys.key, function (resp) {
-		resp.on('data', function(d) {
-			body = body + d;
-		});
-		resp.on('end', function(){
-			var subways = { stations: [] };
-
-			var unfilteredStations = JSON.parse(body).results;
-
-			unfilteredStations.forEach(function(unfilteredStation){
-				unfilteredStation["stations"].push({ name: stationResults.name });
-			});
-
-			hotel['subways'] = subways;
+function promisifyHotelInfoRequests(hotel) {
+	var hotel = hotel,
+		// requestUrl for restaurants
+		requestUrl = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="
+			+ hotel.latitude + "," + hotel.longitude
+			+"&radius=" + 1500
+			+ "&types=" + 'food' +
+			"&key=" + keys.key;
+	return promisifiedHttpsGet(requestUrl)
+		.then(function resolve(stuff) {
+			hotel.restaurants = getRestaurants(stuff);
+			// requestUrl for subways
+			requestUrl = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="
+				+ hotel.latitude + "," + hotel.longitude
+				+"&radius=" + 600
+				+ "&types=" + 'subway_station' +
+				"&key=" + keys.key;
+			return promisifiedHttpsGet(requestUrl)
+		})
+		.then(function resolve(stuff) {
+			console.log('entered subway area')
+			hotel.subways = getSubways(stuff);
 			return hotel;
+		})
+		.catch(function reject(err) {
+			throw new Error(err);
 		});
-	});
-}
-
-
-function promisifyHotelInfoRequests(hotels) {
-		return promisifiedHttpsGet(url + keys.key)
-			.then(function resolve(hotel) {
-				return hotel[getRestaurants(hotel)];
-			}
-			.then(function resolve(hotel) {
-				return storeSubways(hotel);
-			})
-			.catch(function reject(err) {
-				throw new Error(err);
-			});
-	}
 }
 
 
 function getRestaurants(body) {
 	var restaurants = [],
-		unfilteredRestaurants = JSON.parse(body).results,
-		requestUrl = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="
-			+ hotel.latitude + "," + hotel.longitude
-			+"&radius=" + 1500
-			+ "&types=" + 'food' +
-			"&key=";
+		unfilteredRestaurants = JSON.parse(body).results;
 
-	restaurants.forEach(function(unfilteredRestaurant){
-		var restaurant = {};
-		restaurant["restaurantName"] = unfilteredRestaurant["name"];
-		restaurant["priceLevel"] =  unfilteredRestaurant["price_level"];
-		restaurant["rating"] = unfilteredRestaurant["rating"];
+	unfilteredRestaurants.forEach(function(unfilteredRestaurant){
+		var restaurant = {
+			restaurantName: unfilteredRestaurant.name,
+			priceLevel: unfilteredRestaurant.price_level,
+			rating: unfilteredRestaurant.rating
+		};
 		restaurants.push(restaurant);
 	});
 
 	return restaurants;
 }
 
-var promisifiedHttpsGet = Promise.method(function(url, hotel) {
+
+function getSubways(body) {
+	var subways = { stations: [] };
+
+	var unfilteredStations = JSON.parse(body).results;
+
+	unfilteredStations.forEach(function(unfilteredStation){
+		subways["stations"].push({ name: unfilteredStation.name });
+	});
+
+	return subways;
+}
+
+var promisifiedHttpsGet = Promise.method(function(url) {
 	return new Promise(function(resolve, reject) {
 		var body = '';
 		https.get(url, function(res) {
@@ -80,6 +78,7 @@ var promisifiedHttpsGet = Promise.method(function(url, hotel) {
 				body += data;
 			});
 			res.on('end', function success() {
+				// console.log(body, 'that was body')
 				return resolve(body);
 			});
 			res.on('error', function failure(err) {
@@ -92,13 +91,12 @@ var promisifiedHttpsGet = Promise.method(function(url, hotel) {
 // hotels array with necessary keys
 filteredHotels = hotels.map(function(unfilteredHotel) {
 	var hotel = {
-		address: unfilteredHotel.address['addressLine1'],
-		name: unfilteredHotel.name,
+		address: 	unfilteredHotel.location.address['addressLine1'],
+		name: 		unfilteredHotel.name,
 		latitude: unfilteredHotel.location.latitude,
 		longitude: unfilteredHotel.location.longitude,
 		neighborhoodName: unfilteredHotel.location.neighborhoodName,
 		overallGuestRating: unfilteredHotel.overallGuestRating,
-		requestUrl: ,
 		starRating: unfilteredHotel.starRating,
 		thumbnailUrl: unfilteredHotel.thumbnailUrl,
 		totalReviewCount: unfilteredHotel.totalReviewCount,
@@ -107,49 +105,12 @@ filteredHotels = hotels.map(function(unfilteredHotel) {
 	};
 
 	return hotel;	
-}
+});
 
 router.get('/nearby', function(req, res){
-	hotelsPromises = getFoodByHotel(hotels,res);
+	hotelsPromises = filteredHotels.map(promisifyHotelInfoRequests);
 	Promise.all(hotelsPromises)
 		.then(function resolve(hotelsWithInfo) {
-			// res.send("nah");
-			// evalHotelObj(hotels, res);
-			res.send(hotelsWithInfo);
-			res.end();
+			console.log(hotelsWithInfo);
 		});
 });
-
-
-//hard coded
-//different types can be found at https://developers.google.com/places/supported_types
-router.get('/', function(req, res){
-	var longitude = "-74.005475";
-	var latitude  = "40.727583";
-	var address = "181 Varick St, New York";
-	var city = "New York";
-	var radius = "600";
-	var type = "subway_station";//subway_station
-	var testResponse = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + latitude + "," + longitude +"&radius=" + radius + "&types=" + type + "&key=";
-	var body = "";
-	https.get(testResponse + keys.key, function (resp) {
-			var body = "";
-			resp.on('data', function(d) {
-				body = body + d;
-			});
-
-			resp.on('end', function(){
-				res.send(body);
-				res.end();
-			});
-	});
-});
-
-	// var hotel =
-	// 	{ name: 'Courtyard New York Manhattan/Soho',
-	// 	starRating: 3.5,     location:      { address: {addressLine1: "181 Varick St, New York", cityName: "New York"},
-	// 		longitude: -74.005475,        latitude: 40.727583,
-	// 		timeZone: 'America/New_York',        neighborhoodId: '3018',
-	// 		neighborhoodName: 'Soho - Tribeca',        cityId: 3000016152,
-	// 		zoneId: '51958' },     overallGuestRating: 8.9,
-	// 		totalReviewCount: 174, url: "fasfddas", thumbnailUrl : "stff"};
